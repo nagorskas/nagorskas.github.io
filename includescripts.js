0785b25845b6bfa8bfbe341efa145b8a30351c24
nagorskas <79064943+whomsthat@users.noreply.github.com>
2023-06-11 21:36:50 +0300

    Component test

--- includescripts.js
+++ includescripts.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-  $("#navbar").load("navbar.html");
-});
+const navigation = document.getElementById('navigation');
+
+fetch('includes/navbar.html')
+  .then(response => response.text())
+  .then(html => {
+    navigation.innerHTML = html;
+  })
+  .catch(error => {
+    console.error(`Error loading navigation component: ${error}`);
+  });
+
+const copyright = document.getElementById('copyright');
+
+fetch('includes/copy.html')
+  .then(response => response.text())
+  .then(html => {
+    copyright.innerHTML = html;
+  })
+  .catch(error => {
+    console.error(`Error loading navigation component: ${error}`);
+  });
